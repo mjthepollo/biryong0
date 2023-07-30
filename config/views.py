c15466cea89c7d3587ad63922a0c06167643b208
mjthepollo <mjthepollo@gmail.com>
2023-07-30 16:39:38 +0900

    Test before https

--- config/views.py
+++ config/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 
-@login_required
+# @login_required
 def home(request):
     return render(request, "pages/home.html")
     # return redirect(reverse("smalltalk:room", kwargs={"room_name": "lobby"}))
